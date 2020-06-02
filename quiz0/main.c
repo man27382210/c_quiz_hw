4cb307ae471aafe3fb6cd82c2cc02e24ff6be2e7
twtseng <twtseng@indeed.com>
2020-06-02 18:03:13 +0900

    Initial Commit

---
+++ quiz0/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  quiz0
+//
+//  Created by Tai Wei Tseng on 2020/06/02.
+//  Copyright © 2020 man27382210. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
